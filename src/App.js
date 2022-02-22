9096633ced39a3101c569215ba3714ebbaebbbfe
dadasaheb-181097 <dadaso@angularminds.in>
2022-02-22 18:10:13 +0530

    Added Login Functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-
 import './App.css';
+import Login from './Components/Login';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 
 
 function App() {
   return (
     <div className="App">
-     
-      
+      <Router>
+        <Routes>
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
